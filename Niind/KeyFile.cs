d595dadd1770d30a91093405371dd86c80b19bb8
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2021-09-19 20:17:43 +0800

    Fix keyfile format

--- Niind/KeyFile.cs
+++ Niind/KeyFile.cs
@@ -8,31 +8,29 @@ namespace Niind.Structures
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x100)]
         private readonly byte[] Padding;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x14)]
         public byte[] Boot1Hash;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x10)]
         public byte[] CommonKey;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x4)]
         public byte[] ConsoleID;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 30)]
+        // EccPrivateKey and NandHMAC overlaps in between by 2 bytes by some stupid reason...
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x1C)]
         public byte[] EccPrivateKey;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x14)]
         public byte[] NandHMAC;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x10)]
         public byte[] NandAESKey;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x10)]
         public byte[] AES_PRNG_Seed;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
-        public byte[] UnknownKeyID;
-
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 60)]
-        public byte[] UnknownKeySig;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x08)]
+        public byte[] UnknownKeyID; 
     }
 }
